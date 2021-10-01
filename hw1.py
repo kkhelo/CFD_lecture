277a63b3c152c8b9ba0fa360fdcc2fe473e38454
Kevin You <kevinyu8745@gmail.com>
2021-10-01 16:29:50 +0800

    Add some comments

--- hw1.py
+++ hw1.py
@@ -1,16 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
+# Step0 : Set dimension 
 m = 20
 x = np.arange(-0.1, 1.1, 0.05)
 u = np.zeros(m+5)
 u0 = np.zeros(m+5)
 
-DX = 1/m
-CFL = 0.8
-DT = DX * CFL
-
+# def callBC and FDM function in advance
 def callBC():
     u[0] = u[m]
     u[1] = u[m+1]
@@ -21,15 +17,21 @@ def FDM(arr1, arr2):
     for i in range(m+1):
         arr1[i+2] = arr2[i+2] - (DT/DX*(arr2[i+2] - arr2[i+1]))
 
-
+# Step1 : Grid
+DX = 1/m
+CFL = 0.8
+DT = DX * CFL
 for i in range(x.size):
     x[i] = (i-2) * DX
 
+# Step2 : Initial data  
 for i in range(m+1):
     u[i+2] = np.sin(2 * np.pi * x[i+2])
 
+# Step3: B.C.
 callBC()
 
+# Step4 : PDE(FDM), main part of the program 
 t = 0
 while(t <= 2):
     print(t)
@@ -38,9 +40,9 @@ while(t <= 2):
     callBC()
     if(t == 0):
         plt.plot(x,u,'r-*', label = 'u(x,0)')
-    t = round(t + DT, 2)
+    t = round(t + DT, 2)    # Round off the two decimal places
 
-
+# Step5 : Plotting 
 plt.plot(x,u,'b-*', label = 'u(x,2)')
 plt.title("u(x,0) compare to  u(x,2)")
 plt.xlabel("x")
