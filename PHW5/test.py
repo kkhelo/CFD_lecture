69b428e43c1ac77f534559709301e334323336ce
Kevin You <kevinyu8745@gmail.com>
2022-01-19 20:07:02 +0800

    finish all maybe

--- PHW5/test.py
+++ PHW5/test.py
@@ -1,20 +1,27 @@
 
 import numpy as np 
 import matplotlib.pyplot as plt
-P = np.load('P.npy')
+# P = np.load('P.npy')
 
-max = np.max(P)
-min = np.min(P)
-step = (max - min) / 50
+# max = np.max(P)
+# min = np.min(P)
+# step = (max - min) / 50
 
 m = 20
 dx = dy = 1/m
 x = np.arange(0 - 1*dx, 1 + 2*dx, dx)
-y = np.arange(1 + 1*dy, 0 - 2*dy, -dy)
+y = np.arange(0 - 1*dx, 1 + 2*dx, dx)
 X, Y = np.meshgrid(x, y)
 
+# plt.figure()
+# contour = plt.contourf(X, Y, P, levels = np.arange(min, max + step, step))
+# plt.colorbar(contour)
+# plt.savefig('test.png')
+# plt.show()
+
+u = np.load('u.npy')
+v = np.load('v.npy')
+
 plt.figure()
-contour = plt.contourf(X, Y, P, levels = np.arange(min, max + step, step))
-plt.colorbar(contour)
-plt.savefig('test.png')
+streamline = plt.streamplot(X, Y, u, v, density=1)
 plt.show()
